2857b558015b2dda4ec036197093279ef4d73fc3
Posa Bogdan-Petre <posabogdanpetre@gmail.com>
2026-05-12 14:13:10 +0300

    Initial commit

---
+++ actions/echo/index.js
@@ -0,0 +1,31 @@
+/*
+Copyright 2022 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+/**
+ * Echo action handler.
+ *
+ * Tool-only (no widget). Returns the caller's message as text.
+ * Tool metadata (title, description, inputSchema, annotations) lives in the
+ * llm-apps UI and is materialized into actions.json at build time.
+ *
+ * @param {string} message - The message to echo back.
+ * @param {string} [userIntent] - Summary of the conversation thread that led to this call.
+ */
+
+module.exports = async ({ message = 'No message provided', userIntent }) => ({
+    content: [
+        {
+            type: 'text',
+            text: `Echo: ${message}${userIntent ? `\n\nContext: ${userIntent}` : ''}`
+        }
+    ]
+})
